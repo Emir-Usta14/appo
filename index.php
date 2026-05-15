39ebf882a69e5a43f8a07a5500e1335898f7791f
Emir-Usta14 <ustae@farmingdale.edu>
2026-05-14 23:22:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz App</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
+<div class="container">
+
+<h1>Quiz Application</h1>
+
+<p>
+Test your knowledge and compete on the leaderboard.
+</p>
+
+<br>
+
+<a href="login.php">
+    <button>Sign In</button>
+</a>
+
+<br><br>
+
+<a href="signup.php">
+    <button>Create Account</button>
+</a>
+
+</div>
+
+</body>
+</html>
