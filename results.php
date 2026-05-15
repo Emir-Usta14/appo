3d4968638b7320a839415a73be3dbceb21778fc2
Emir-Usta14 <ustae@farmingdale.edu>
2026-05-14 23:35:54 -0400

    Update results.php

--- results.php
+++ results.php
@@ -7,4 +7,83 @@ if (!isset($_SESSION["username"])) {
     exit();
 }
 
-$
+$score = 0;
+
+for ($i = 1; $i <= 10; $i++) {
+
+    if (
+        isset($_POST["q$i"]) &&
+        isset($_POST["correct$i"])
+    ) {
+
+        if ($_POST["q$i"] == $_POST["correct$i"]) {
+            $score++;
+        }
+    }
+}
+
+$username = $_SESSION["username"];
+
+$conn->query("
+    INSERT INTO scores(username, score)
+    VALUES('$username', '$score')
+");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Results</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+<h1>Your Score</h1>
+
+<div class="question-box">
+
+<h2>
+You scored <?php echo $score; ?>/10
+</h2>
+
+</div>
+
+<br>
+
+<a href="quiz.php">
+<button class="submit-btn">
+Play Again
+</button>
+</a>
+
+<br><br>
+
+<a href="leaderboard.php">
+<button class="submit-btn">
+Leaderboard
+</button>
+</a>
+
+<br><br>
+
+<a href="profile.php">
+<button class="submit-btn">
+Profile
+</button>
+</a>
+
+<br><br>
+
+<a href="logout.php">
+<button class="submit-btn">
+Logout
+</button>
+</a>
+
+</div>
+
+</body>
+</html>
