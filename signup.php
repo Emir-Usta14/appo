7f981ae31afe9033795a431323e69773f0e89584
Emir-Usta14 <ustae@farmingdale.edu>
2026-05-14 23:18:08 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,1 +1,89 @@
 
+<?php
+session_start();
+include "db.php";
+
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
+
+    $check = $conn->query("
+        SELECT *
+        FROM users
+        WHERE username='$username'
+    ");
+
+    if ($check->num_rows > 0) {
+
+        $error = "Username already exists";
+
+    } else {
+
+        $conn->query("
+            INSERT INTO users(username,password,is_admin)
+            VALUES('$username','$password',0)
+        ");
+
+        header("Location: login.php");
+        exit();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+<h1>Create Account</h1>
+
+<form method="POST">
+
+<input
+type="text"
+name="username"
+placeholder="Username"
+required
+>
+
+<br><br>
+
+<input
+type="password"
+name="password"
+placeholder="Password"
+required
+>
+
+<br><br>
+
+<button type="submit">
+Create Account
+</button>
+
+</form>
+
+<p style="color:red;">
+<?php echo $error; ?>
+</p>
+
+<br>
+
+<a href="login.php">
+<button type="button">
+Back To Login
+</button>
+</a>
+
+</div>
+
+</body>
+</html>
